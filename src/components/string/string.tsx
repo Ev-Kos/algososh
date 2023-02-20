a6d9795e18a892f0ee469ccc918f59f6d0a64d60
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-02-20 10:09:22 +0300

    layout

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from './string.module.css';
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styles.form}>
+        <Input isLimitText={true} maxLength={11}></Input>
+        <Button text='Развернуть' type="submit"></Button>
+      </form>
     </SolutionLayout>
   );
 };
