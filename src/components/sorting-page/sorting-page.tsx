7531578efbdd16eb9a8faf3a50d88a7864c58a7e
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-02-22 13:25:55 +0300

    add selectionSortDes

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,18 +1,22 @@
-import { useState, useEffect, FormEvent } from "react";
+import { FC, useState, useEffect, FormEvent } from "react";
 import { Button } from "../ui/button/button";
 import { Column } from "../ui/column/column";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from './sorting-page.module.css';
 import { ElementStates } from "../../types/element-states";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { Direction } from "../../types/direction";
 
 type TArr = {
   value: number;
-  color: ElementStates;
+  color?: ElementStates;
 }
 
-export const SortingPage: React.FC = () => {
+export const SortingPage: FC = () => {
   const [arr, setArr] = useState<TArr[]>([]);
+  const [nameSort, setNameSort] = useState<string>('choice');
+  const [loader, setLoader]= useState(false);
 
   const randomArr = () => {
     const randomNumber = (minLen: number, maxLen: number) => {
@@ -24,12 +28,12 @@ export const SortingPage: React.FC = () => {
       .fill(null)
       .map(() => Math.floor(Math.random() * 100));
 
-    const arr = randomArr.map((value) => ({
+    const array = randomArr.map((value) => ({
       value,
       color: ElementStates.Default,
     }));
 
-    setArr(arr);
+    setArr(array);
   };
 
   useEffect(() => {
@@ -40,19 +44,54 @@ export const SortingPage: React.FC = () => {
     e.preventDefault();
     randomArr();
   };
+
+  const swap = (arr: TArr[], firstIndex: number, secondIndex: number): void => {
+    const temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+  };
+
+  const delay = (time: number) =>
+    new Promise((resolve) => setTimeout(resolve, time));
   
+  //по убыванию
+  const selectionSortDes = async (arr: TArr[]) => {
+    setLoader(true)
+    const { length } = arr;
+    for (let i = 0; i < length - 1; i++) {
+      let maxInd = i;
+      for (let j = i + 1; j < length; j++) {
+        arr[i].color = ElementStates.Changing;
+        arr[j].color = ElementStates.Changing;
+        setArr([...arr]);
+        await delay(SHORT_DELAY_IN_MS)
+        if (arr[j].value > arr[maxInd].value) {
+          maxInd = j;   
+        }
+        arr[j].color = ElementStates.Default;
+        setArr([...arr]);
+      }
+      swap(arr, i, maxInd);
+      arr[i].color = ElementStates.Modified;
+    }
+    arr[length - 1].color = ElementStates.Modified;
+    setArr([...arr]);
+    setLoader(false)
+  }
+
+
   return (
     <SolutionLayout title="Сортировка массива">
       <form className={styles.form}>
         <div className={styles.radioButtons}>
-          <RadioInput label="Выбор" value='choice' name="choice"/>
-          <RadioInput label="Пузырёк" value='bubble' name="bubble"/>
+          <RadioInput label="Выбор" checked={nameSort === 'choice' ? true : false}/>
+          <RadioInput label="Пузырёк" checked={nameSort === 'bubble' ? true : false}/>
         </div>
         <div className={styles.sortButtons}>
           <Button type="button" text="По возрастанию" extraClass='ascending'/>
-          <Button type="button" text="По убыванию" extraClass='descending'/>
+          <Button type="button" text="По убыванию" extraClass='descending' sorting={Direction.Descending} onClick={()=>selectionSortDes(arr)} isLoader={loader}/>
         </div>
-        <Button type="submit" text="Новый массив" onClick={handleClick}/>
+        <Button type="submit" text="Новый массив" onClick={()=>handleClick}/>
       </form>
       <div  className={styles.result}>
       {arr.map((item, index) => {
