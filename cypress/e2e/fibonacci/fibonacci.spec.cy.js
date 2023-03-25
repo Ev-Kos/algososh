804c10169a2632936f0c9e899b45ab95f1102398
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-03-25 16:32:29 +0300

    add e2e to stack

--- cypress/e2e/fibonacci/fibonacci.spec.cy.js
+++ cypress/e2e/fibonacci/fibonacci.spec.cy.js
@@ -1,8 +1,5 @@
-import {DELAY_IN_MS} from '../../../src/constants/delays';
-
 describe('Тестирование последовательности Фибоначчи', () => {
     const result = [1, 1, 2, 3, 5, 8];
-    const colorDefault = "rgb(0, 50, 255)";
     beforeEach(() => {
         cy.visit('http://localhost:3000/fibonacci');
         cy.get('input[type="number"]').as('input');
@@ -20,11 +17,11 @@ describe('Тестирование последовательности Фибо
         cy.get('@button').should('be.not.disabled').click();
 
         for (let i = 0; i < result.length; i++) {
-             cy.get('div[class*=result]')
-               .children()
-               .last()
-               .find('div[class*="circle_circle"]')
-               .should('have.text', result[i]);
+            cy.get('div[class*=result]')
+                .children()
+                .last()
+                .find('div[class*="circle_circle"]')
+                .should('have.text', result[i]);
         }    
     });
 })
